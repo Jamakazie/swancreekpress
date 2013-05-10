3dbc9e3161dc99e44dd1243cdf4c26fe28450274
jama <jamakazie@gmail.com>
2013-05-10 14:08:31 -0400

    intial commit

---
+++ main/views.py~
@@ -0,0 +1,7 @@
+from django.shortcuts import render,render_to_response
+from django.template import RequestContext
+
+# Create your views here.
+def index(request):
+	context = {}
+	return render_to_response('index.html', context, context_instance = RequestContext(request))
---
+++ swancreekpress/settings.py
@@ -0,0 +1,99 @@
+"""
+Django settings for swancreekpress project.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/dev/topics/settings/
+
+For the full list of settings and their values, see
+https://docs.djangoproject.com/en/dev/ref/settings/
+"""
+
+# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+import os
+BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+
+
+# Quick-start development settings - unsuitable for production
+# See https://docs.djangoproject.com/en/dev/howto/deployment/checklist/
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = '$5^s7s)s@uj^i!0yzmwilrfh6naq7leweb@p#he@lol%$85e!('
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+TEMPLATE_DEBUG = True
+ 
+TEMPLATE_DIRS = (
+	BASE_DIR + "/templates",
+	BASE_DIR + "/main",
+	BASE_DIR + "/author"
+)
+
+ALLOWED_HOSTS = []
+
+
+# Application definition
+
+INSTALLED_APPS = (
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'main',
+    'author',
+)
+
+STATIC_ROOT = BASE_DIR + '/static/'
+
+STATICFILES_DIRS = (
+	BASE_DIR + '/templates/static/',
+	BASE_DIR + '/main/static/',
+	BASE_DIR + '/author/static',
+)
+STATIC_URL = '/static/'
+
+MIDDLEWARE_CLASSES = (
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+)
+
+ROOT_URLCONF = 'swancreekpress.urls'
+
+WSGI_APPLICATION = 'swancreekpress.wsgi.application'
+
+
+# Database
+# https://docs.djangoproject.com/en/dev/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
+
+# Internationalization
+# https://docs.djangoproject.com/en/dev/topics/i18n/
+
+LANGUAGE_CODE = 'en-us'
+
+TIME_ZONE = 'UTC'
+
+USE_I18N = True
+
+USE_L10N = True
+
+USE_TZ = True
+
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/dev/howto/static-files/
+
+STATIC_URL = '/static/'
